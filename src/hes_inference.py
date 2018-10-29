e73da575773c16282d738c73e1c65197f06d14db
Jochen Kursawe <jochen.kursawe@manchester.ac.uk>
2018-10-29 17:47:18 +0000

    starting to implement a Kalman filter

--- src/hes_inference.py
+++ src/hes_inference.py
@@ -1,1 +1,34 @@
 ## need to write code here
+def kalman_filter(protein_at_observation,model_parameters):
+    """
+    Perform Kalman-Bucy filter based on observation of protein
+    copy numbers. This implements the filter described by Calderazzo et al., Bioinformatics (2018).
+    
+    Parameters
+    ----------
+    
+    protein_at_observation : numpy array.
+        Observed protein. The dimension is n x 2, where n is the number of observation time points.
+        The first column is the time, and the second column is the observed protein copy number at
+        the corresponding time.
+    
+    model_parameters : numpy array.
+        An array containing the model parameters in the following order.
+        repression_threshold, hill_coefficient, mRNA_degradation_rate,
+        protein_degradation_rate, basal_transcription_rate, translation_rate,
+        transcription_delay.
+    
+    Returns
+    -------
+    
+    state_space_mean : numpy array.
+        An array of length n, which gives the number of observation time points.
+        The first column is time, the second column is the mean mRNA, and the third
+        column is the mean protein.
+    
+    state_space_variance : numpy array.
+        An array of dimension 2n x 2n. 
+              [ cov( mRNA(t0:tn),mRNA(t0:tn) ),    cov( protein(t0:tn),mRNA(t0:tn) ),
+                cov( mRNA(t0:tn),protein(t0:tn) ), cov( protein(t0:tn),protein(t0:tn) ]
+    
+    """
--- test/test_inference.py
+++ test/test_inference.py
@@ -18,5 +18,25 @@ import hes_inference
 class TestInference(unittest.TestCase):
 
     def test_inference(self):
-        ## write a test here!
+        ## run a sample simulation to generate example protein data
+        true_data = hes5.generate_langevin_trajectory(duration = 900)
+        
+        ## the F constant matrix is left out for now
+        protein_at_observation = true_data[0:900:10,(0,2)]
+        protein_at_observation[1] += np.random.randn(90)
+            
+        parameters = [720, 10000,5,np.log(2)/30, np.log(2)/90, 1, 1, 29]
+         
+        ## apply kalman filter to the data
+        state_space_mean, state_space_variance = hes_inference.kalman_filter(protein,
+                                                                             parameters)
+        
+        self.assertEqual(state_space_mean.shape[0],90)
+        self.assertEqual(state_space_mean.shape[1],3)
+        self.assertEqual(state_space_variance.shape[0],180)
+        self.assertEqual(state_space_variance.shape[1],180)
+        # check dimensionality of state_space_mean and the state_space_variance
+        # variance needs to be positive definite and symmetric, maybe include quantitative check
+        
+        ##plot data together with state-space distribution
         pass
