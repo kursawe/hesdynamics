b08f6f755943feca43e959bcca34b5bdfd6fa8dd
burtonjosh <josh@burton-online.me.uk>
2018-11-05 11:12:37 +0000

    added variance to kalman prediction step

--- src/hes_inference.py
+++ src/hes_inference.py
@@ -78,10 +78,10 @@ def kalman_prediction_step(state_space_mean,state_space_variance,model_parameter
         repression_threshold, hill_coefficient, mRNA_degradation_rate,
         protein_degradation_rate, basal_transcription_rate, translation_rate,
         transcription_delay.
-    
+
     observation_time_step : float.
         This gives the time between each experimental observation, and allows us to calculate the number of
-        discretisation time steps required for the forward Euler scheme. 
+        discretisation time steps required for the forward Euler scheme.
 
     Returns
     -------
@@ -92,13 +92,14 @@ def kalman_prediction_step(state_space_mean,state_space_variance,model_parameter
     predicted_state_space_variance : numpy array.
     The dimension is 2n x 2n, where n is the number of previous observations until the current time.
         [ cov( mRNA(t0:tn),mRNA(t0:tn) ),    cov( protein(t0:tn),mRNA(t0:tn) ),
-          cov( mRNA(t0:tn),protein(t0:tn) ), cov( protein(t0:tn),protein(t0:tn) ]
+          cov( mRNA(t0:tn),protein(t0:tn) ), cov( protein(t0:tn),protein(t0:tn) ]number_of_obse
 
     """
-    
+
     discretisation_time_step = 1.0
     number_of_hidden_states = int(observation_time_step/discretisation_time_step)
-    
+    number_of_observations = len(state_space_mean[:,0])
+
     repression_threshold = model_parameters[0]
     hill_coefficient = model_parameters[1]
     mRNA_degradation_rate = model_parameters[2]
@@ -106,19 +107,34 @@ def kalman_prediction_step(state_space_mean,state_space_variance,model_parameter
     basal_transcription_rate = model_parameters[4]
     translation_rate = model_parameters[5]
     transcription_delay = model_parameters[6]
-    
+
     discrete_delay = int(np.around(transcription_delay/discretisation_time_step))
-        
+
+    ## need to define P(t,s) and P(s,t) to use in the calculation of the variance.
+
     for time_index in range(number_of_hidden_states):
-        
+
         hill_function_value = 1.0/(1.0+np.power(state_space_mean[time_index-discrete_delay,2]/repression_threshold,hill_coefficient))
-        
-        state_space_mean[time_index+1,(1,2)] = (state_space_mean[time_index,(1,2)] + state_space_mean[time_index,(1,2)].dot(
+        hill_function_derivative_value = (-hill_coefficient/repression_threshold)*np.power(1+(state_space_mean[time_index-discrete_delay,2]/repression_threshold),-(hill_coefficient+1))
+
+        state_space_mean[time_index+1,(1,2)] = (state_space_mean[time_index,(1,2)] + number_of_hidden_states*(state_space_mean[time_index,(1,2)].dot(
                                                                                         np.array([[-mRNA_degradation_rate,translation_rate],
                                                                                                  [0,-protein_degradation_rate]]))
-                                                                                   + np.array([basal_transcription_rate*hill_function_value,0])
-                                            
-                                            
+                                                                                   + np.array([basal_transcription_rate*hill_function_value,0])))
+
+        state_space_variance[(time_index+1,time_index+number_of_observations+1),(time_index+1,time_index+number_of_observations+1)] = (
+
+            state_space_variance[(time_index,time_index+number_of_observations),(time_index,time_index+number_of_observations)] + number_of_hidden_states*(
+                np.array([[-mRNA_degradation_rate,0],[translation_rate,-protein_degradation_rate]]).dot(state_space_variance[(time_index,time_index+number_of_observations),(time_index,time_index+number_of_observations)])
+                + np.transpose(state_space_variance[(time_index,time_index+number_of_observations),(time_index,time_index+number_of_observations)]).dot(np.array([[-mRNA_degradation_rate,translation_rate],
+                         [0,-protein_degradation_rate]]))
+                + np.array([[0,hill_function_derivative_value],[0,0]])*P(s,t) + P(s,t)*np.array([[0,0],[basal_transcription_rate*hill_function_derivative_value]])
+                + np.array([[mRNA_degradation_rate*state_space_mean[time_index,1]+basal_transcription_rate*hill_function_value,0],
+                            [0,translation_rate*state_space_mean[time_index,1]+protein_degradation_rate*state_space_mean[time_index,2]]])
+            )
+        )
+
+
 def kalman_update_step(predicted_state_space_mean, predicted_state_space_variance,current_observation,time_delay):
     """
     Perform the Kalman filter update step on the predicted mean and variance, given a new observation.
@@ -172,12 +188,13 @@ def kalman_update_step(predicted_state_space_mean, predicted_state_space_varianc
 
     observation_time_step = state_space_mean[1,0] - state_space_mean[0,0]
     maximum_delay_index = int(math.ceil(time_delay/observation_time_step))
+    number_of_observations = len(state_space_mean[:,0])
 
     # This is F in the paper
     observation_transform = np.array([0,1])
     observation_variance = 0.1
     helper_inverse = 1.0/(observation_transform.dot(state_space_variance[(number_of_observations-1,-1),
-                                                     (number_of_observations-1,-1)].dot(transpose(observation_transform)))
+                                                     (number_of_observations-1,-1)].dot(np.transpose(observation_transform)))
                                                      +observation_variance)
 
     # need to define C (the coefficient of adaptation) somewhere
