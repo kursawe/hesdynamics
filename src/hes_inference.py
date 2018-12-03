9e79171ee78e4e676dadd873e6f1e946c988d762
burtonjosh <josh@burton-online.me.uk>
2018-12-03 16:57:47 +0000

    more tuning

--- src/hes_inference.py
+++ src/hes_inference.py
@@ -548,10 +548,10 @@ def kalman_random_walk(iterations,protein_at_observations,hyper_parameters,measu
     if kwargs.get("adaptive") == "true":
         for i in range(1,iterations):
             print(cholesky_covariance)
-            if i >= 20:
+            if i >= 50:
                 acceptance_tuner = 0.8
-                if np.mod(i,10) == 0:
-                    parameter_covariance = np.cov(random_walk[10:i,].T) + 0.000001*identity
+                if np.mod(i,20) == 0:
+                    parameter_covariance = np.cov(random_walk[40:i,].T) + 0.000001*identity
                     cholesky_covariance  = np.linalg.cholesky(parameter_covariance)
 
             new_state = current_state + acceptance_tuner*cholesky_covariance.dot(multivariate_normal.rvs(size=7))
