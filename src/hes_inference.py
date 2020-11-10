901f2cc05877d7371d84c6c451f8ae2a7e90f3c7
Josh Burton <josh@burton-online.me.uk>
2020-11-10 12:52:54 +0000

    random walk tuning fixed

--- src/hes_inference.py
+++ src/hes_inference.py
@@ -1650,7 +1650,7 @@ def kalman_random_walk(iterations,protein_at_observations,hyper_parameters,measu
     # LAP parameters
     k = 1
     c0 = 1.0
-    c1 = 0.7
+    c1 = 0.8
 
     cholesky_covariance = np.linalg.cholesky(proposal_covariance)
 
@@ -1718,7 +1718,7 @@ def kalman_random_walk(iterations,protein_at_observations,hyper_parameters,measu
                 acceptance_count += 1
 
             # LAP stuff
-            if step_index%k == 0 and step_index > int(0.1*iterations) and step_index < int(3*iterations/4):
+            if step_index%k == 0 and step_index > 1 and step_index < int(3*iterations/4):
                 # r_hat = acceptance_count/step_index
                 block_sample = random_walk[:step_index,[0,1,4,5,6]]
                 block_proposal_covariance = np.cov(block_sample.T)
--- test/test_inference.py
+++ test/test_inference.py
@@ -1252,7 +1252,7 @@ class TestInference(unittest.TestCase):
         print(random_walk)
         print(acceptance_rate)
 
-    def xest_kalman_random_walk(self,data_filename='protein_observations_ps11_ds4.npy'):
+    def test_kalman_random_walk(self,data_filename='protein_observations_ps11_ds4.npy'):
         # load data and true parameter values
         saving_path = os.path.join(os.path.dirname(__file__),'data','')
         protein_at_observations = np.load(os.path.join(saving_path,data_filename))
@@ -1418,7 +1418,7 @@ class TestInference(unittest.TestCase):
         #self.assertEqual(array_of_random_walks.shape[0], len(initial_states))
         #self.assertEqual(array_of_random_walks.shape[1], number_of_iterations)
 
-    def test_multiple_mala_traces_figure_5(self,data_filename = 'protein_observations_ps6_fig5_3.npy'):
+    def xest_multiple_mala_traces_figure_5(self,data_filename = 'protein_observations_ps6_fig5_3.npy'):
         # load data and true parameter values
         saving_path = os.path.join(os.path.dirname(__file__),'data','')
         protein_at_observations = np.array([np.load(os.path.join(saving_path,data_filename))])
